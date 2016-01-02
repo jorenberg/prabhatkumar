9154c0bda75774120cbbd42046034a30e519a14c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-01-02 18:34:23 +0530

    Initial commit
    init.sh, shell script to --/initialize/-- whole "prabhatkumar.org®" in one go.
    Usage: $ sh init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# ======================== prabhatkumar.org® =========================
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Prabhat Kumar. All rights reserved.
+# ====================================================================
