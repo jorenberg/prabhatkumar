1152d3960c659a75f147ca5809dcc4e79d0649b5
Prabhat Kumar <prabhat.genome@gmail.com>
2016-01-02 18:28:08 +0530

    Initial commit
    --/The Heart of Test System/-- of "sequomics®".
    karma, The Spectacular Test Runner for JavaScript.
    Help: https://github.com/karma-runner/karma
    Usage: $ karma init

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+
+/*!
+ * prabhatkumar.org® [v1.0.0]
+ * ___________________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ___________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/]
+ * ___________________________________________________________________
+ * @date      : 01-Jan-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ___________________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "prabhatkumar.org®".
+ * ___________________________________________________________________
+ */
