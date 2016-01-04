6ade216099d25a31d7e1d5712c27e2ce11f180a9
Prabhat Kumar <prabhat.genome@gmail.com>
2016-01-04 16:16:54 +0530

    Initial commit
    Initials of Build System of prabhatkumar.org®.
    Help: http://compass-style.org/.

--- config.rb
+++ config.rb
@@ -1,1 +1,18 @@
 #!/usr/bin/env ruby
+
+
+# AXIX®-Pro
+# From the Desk of Prabhat Kumar.
+# @author     : Prabhat Kumar
+# @copyright  : Prabhat Kumar
+# @homepage   : http://prabhatkumar.org/
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Everest
+# @date       : 04-Jan-2015
+# @license    : Apache, version 2.0
+# ----------------------------------------
+# Principle   : Standard Module Definition
+# ----------------------------------------
