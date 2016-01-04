287e3cbe37ebe788161896b2284fd5373c439f6b
Prabhat Kumar <prabhat.genome@gmail.com>
2016-01-04 14:55:01 +0530

    Initial commit
    Port Settings for Build System/-- of "prabhatkumar.org®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,3 +57,10 @@
 
 /* Invoking strict mode. */
 "use strict";
+
+/* ______________________________________________
+ * Settings for Build System.                   |
+ * _____________________________________________|
+ */
+var SERVER_PORT     = 9000;
+var LIVERELOAD_PORT = 35729;
